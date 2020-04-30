17b144a3e105d4b55f745c6faa24ba6a0ac87db5
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2020-04-30 16:58:44 +1000

    add styling to card

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import './App.css';
+import './css/reset.css';
+import './css/App.css';
 import Cards from './Components/Cards'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        header
       </header>
       <Cards />
     </div>
--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -16,15 +16,62 @@ interface iProps {
   type: string;
 }
 
-const Card: FunctionComponent<iProps> = props => {
+const Card: FunctionComponent<iProps> = props => { 
 
   return (
-    <div className="job-card">
-      <div className="job-image">
-        <img src={props.image} alt={props.company} />
+    <div className={"job-card" + (props.featured ? " cyan-left-border" : "")}>
+      <div className="job">
+        <div className="job-image">
+          <img src={props.image} alt={props.company} />
+        </div>
+        <div className="job-details">
+          <div className="company">
+            {props.company &&
+              <span className="company-name">{props.company}</span>
+            }
+            {props.new &&
+              <span className="new-tag">NEW!</span>
+            }
+            {props.featured &&
+              <span className="featured-tag">FEATURED</span>
+            }
+          </div>
+          {props.jobtitle &&
+            <h2>{props.jobtitle}</h2>
+          }
+          <div className="job-info">
+            {props.posted &&
+              <span>{props.posted}</span>
+            }
+            <span>&#8226;</span>
+            {props.type &&
+              <span>{props.type}</span>
+            }
+            <span>&#8226;</span>
+            {props.location &&
+              <span>{props.location}</span>
+            }
+          </div>
+        </div>
       </div>
-      
-
+      <div className="job-tags">
+        {props.role && 
+          <span>{props.role}</span>
+        }
+        {props.level && 
+          <span>{props.level}</span>
+        }
+        {props.languages && 
+          props.languages.map((language) => (
+            <span>{language}</span>
+          ))
+        }
+        {props.tools &&
+          props.tools.map((tool) => (
+            <span>{tool}</span>
+          ))
+        }
+      </div>
     </div>
   )
 }
